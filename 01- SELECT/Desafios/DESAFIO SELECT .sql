ef272e16a1cdf20dd850c128734113c912ee80db
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 08:24:04 -0300

    Delete 01- SELECT directory

--- 01- SELECT/Desafios/DESAFIO SELECT .sql
+++
@@ -1,8 +0,0 @@
---°DESAFIO SELECT
---°SELECIONE AS COLUNAS DE NOME, SOBRENOME E DEPARTAMENTO E AS NOMEIE.
-
-USE ContosoRetailDW
-	SELECT FirstName,
-			LastName,
-			DepartmentName
-		FROM DimEmployee
