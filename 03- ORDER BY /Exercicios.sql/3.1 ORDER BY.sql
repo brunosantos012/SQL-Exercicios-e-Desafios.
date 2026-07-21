fcc265aea55dd5b05bef2f9fa6c306df6013ab6f
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 11:01:11 -0300

    Update 3.1 ORDER BY.sql

--- 03- ORDER BY /Exercicios.sql/3.1 ORDER BY.sql
+++ 03- ORDER BY /Exercicios.sql/3.1 ORDER BY.sql
@@ -1,4 +1,8 @@
+--°Listar o nome, a marca e o preço de todos os produtos da tabela DimProduct.
+--°Renomeando as colunas para facilitar a leitura e organizando os resultados do menor para o maior preço.
+
 USE ContosoRetailDW
+
 SELECT ProductName AS 'NOME DO PRODUTO',
        BrandName AS 'MARCA',
        UnitPrice AS 'PREÇO'
