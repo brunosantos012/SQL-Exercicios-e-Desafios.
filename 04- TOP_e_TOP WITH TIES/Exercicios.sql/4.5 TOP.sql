e74e3701e58e1c6cce160dd43d1de0772a98c112
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 11:35:26 -0300

    Update 4.5 TOP.sql

--- 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.5 TOP.sql
+++ 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.5 TOP.sql
@@ -1,3 +1,5 @@
+--°Exibir os 8 primeiros funcionários cadastrados na tabela DimEmployee, apresentando o primeiro nome, sobrenome e departamento em que atuam.
+
 USE ContosoRetailDW
 
 SELECT TOP (8)
