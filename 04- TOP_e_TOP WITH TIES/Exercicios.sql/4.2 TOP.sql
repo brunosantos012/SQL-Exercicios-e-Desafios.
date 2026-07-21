cc9fcf8dab1a2405960ae2012a50d65c428c78a1
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 11:28:26 -0300

    Update 4.2 TOP.sql

--- 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.2 TOP.sql
+++ 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.2 TOP.sql
@@ -1,5 +1,6 @@
-USE ContosoRetailDW
+--°Exibir os 5 produtos mais caros da tabela DimProduct, apresentando o nome do produto, a marca e o preço unitário.
 
+USE ContosoRetailDW
 
 SELECT TOP (5)
        ProductName,
