56bafe6fc016517ade2605926ae9fd0ecd728385
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 11:27:19 -0300

    Update 4.1 TOP.sql

--- 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.1 TOP.sql
+++ 04- TOP_e_TOP WITH TIES/Exercicios.sql/4.1 TOP.sql
@@ -1,5 +1,6 @@
-USE ContosoRetailDW
+--°Exibir apenas os 10 primeiros registros da tabela DimProduct, retornando o nome do produto, a marca e o preço unitário.
 
+USE ContosoRetailDW
 
 SELECT TOP (10)
        ProductName,
