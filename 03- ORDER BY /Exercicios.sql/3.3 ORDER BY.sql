13edaeb91e0b650e825e6fe6abcffb13aa307ab0
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 11:05:45 -0300

    Update 3.3 ORDER BY.sql

--- 03- ORDER BY /Exercicios.sql/3.3 ORDER BY.sql
+++ 03- ORDER BY /Exercicios.sql/3.3 ORDER BY.sql
@@ -1,4 +1,8 @@
+--°Listar o nome, sobrenome e departamento de cada funcionário, organizando os resultados em ordem alfabética
+--pelo nome do departamento para facilitar a visualização da estrutura organizacional.
+
 USE ContosoRetailDW
+
 SELECT FirstName AS 'NOME',
        LastName AS 'SOBRENOME',
        DepartmentName AS 'DEPARTAMENTO' 
