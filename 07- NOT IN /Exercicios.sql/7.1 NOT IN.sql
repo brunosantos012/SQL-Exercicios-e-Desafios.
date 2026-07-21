c2f6d5cd93ed73ca01fa5f69210976ed0d34437a
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 15:49:08 -0300

    Update 7.1 NOT IN.sql

--- 07- NOT IN /Exercicios.sql/7.1 NOT IN.sql
+++ 07- NOT IN /Exercicios.sql/7.1 NOT IN.sql
@@ -1,3 +1,5 @@
+--°Listar os produtos que não pertencem às marcas CONTOSO e FABRIKAM, exibindo o nome do produto, a marca e o preço unitário, organizados do maior para o menor preço.
+
 USE ContosoRetailDW
 
 SELECT 
