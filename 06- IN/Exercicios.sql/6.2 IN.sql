d1083ad9c2c91a1889a25ec4477c963ccf8db479
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 14:32:12 -0300

    Update 6.2 IN.sql

--- 06- IN/Exercicios.sql/6.2 IN.sql
+++ 06- IN/Exercicios.sql/6.2 IN.sql
@@ -1,4 +1,8 @@
+--°Listar os funcionários cujo estado civil seja Casado (M) ou Solteiro (S), exibindo o nome, sobrenome e estado civil com os resultados organizados em ordem alfabética.
+-- Com os resultados organizados em ordem alfabética.
+
 USE ContosoRetailDW
+
 SELECT 
      FirstName AS'NOME',
      LastName AS'SOBRENOME',
