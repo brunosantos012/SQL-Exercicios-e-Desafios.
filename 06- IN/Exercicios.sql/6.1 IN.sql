39ed32cc2a2ac23601b16ecebfab07e2d91c13a6
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 14:29:46 -0300

    Update 6.1 IN.sql

--- 06- IN/Exercicios.sql/6.1 IN.sql
+++ 06- IN/Exercicios.sql/6.1 IN.sql
@@ -1,4 +1,7 @@
+--°Listar os produtos pertencentes às marcas CONTOSO, FABRIKAM e LITWARE, exibindo o nome do produto, a marca e o preço unitário.
+
 USE ContosoRetailDW
+
 SELECT
      ProductName,
      BrandName,
