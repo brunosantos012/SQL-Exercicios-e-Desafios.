c7ea46ead5a0f1176554a1aed4de5b9356663910
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 15:50:26 -0300

    Update 7.2 NOT IN.sql

--- 07- NOT IN /Exercicios.sql/7.2 NOT IN.sql
+++ 07- NOT IN /Exercicios.sql/7.2 NOT IN.sql
@@ -1,3 +1,6 @@
+--°Listar os produtos que não possuem as cores Black e Silver, exibindo o nome do produto, a cor, a marca e o preço unitário 
+-- Organizados em ordem alfabética pelo nome do produto.
+
 USE ContosoRetailDW
 
 SELECT
