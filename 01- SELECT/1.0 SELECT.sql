555081aa4042cf2a9052ed95e8061a3530a8e05a
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 07:33:49 -0300

    Add files via upload

---
+++ 01- SELECT/1.0 SELECT.sql
@@ -0,0 +1,27 @@
+--Exercício 1 - SELECT *
+
+--Objetivo: visualizar dados.
+--Liste todos os produtos.
+--Liste todos os clientes.
+--Liste todos os funcionários.
+--Liste todas as lojas.
+
+USE ContosoRetailDW
+	SELECT * 
+		FROM DimProduct
+	
+	SELECT *
+		FROM DimCustomer
+
+	SELECT *
+		FROM DimStore
+
+	SELECT *
+		FROM DimEmployee
+
+	SELECT * 
+		FROM FactSales
+
+
+
+		
