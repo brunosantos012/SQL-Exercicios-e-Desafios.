b53771935f994e23b5c845c0b277a990e262c6ae
BrunoSantosmr <brunobahia56@hotmail.com>
2026-07-21 09:52:12 -0300

    Update 2.2 WHERE.sql

--- 02- WHERE/Exercicios.sql/2.2 WHERE.sql
+++ 02- WHERE/Exercicios.sql/2.2 WHERE.sql
@@ -1,6 +1,16 @@
+--Exercício:
+--Na tabela: DimEmployee
+--Mostre:
+--Nome
+--Sobrenome
+--Departamento
+
+--Somente funcionários do departamento:
+
 USE ContosoRetailDW
-SELECT FirstName AS 'NOME',
-       LastName AS 'SOBRENOME',
-       DepartmentName
+
+       SELECT FirstName AS 'NOME',
+              LastName AS 'SOBRENOME',
+              DepartmentName AS 'DEPARTAMENTO'
 FROM DimEmployee
     WHERE DepartmentName = 'SALES'
